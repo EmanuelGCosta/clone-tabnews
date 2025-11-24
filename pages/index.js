3f1d23ee2476dcbf2114d3405630863b2d61cadc
EmanuelGCosta <emanuelgs3.abec@gmail.com>
2025-11-24 16:41:02 -0300

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Estou ficando insano</h1>;
+  return <h1>Estou ficando insano</h1>;
+}
+
+function Home() {
+  return <h1>Estou ficando insano</h1>;
 }
 
 export default Home;
